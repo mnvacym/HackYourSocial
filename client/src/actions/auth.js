c325446eae8be26c1822c577fea6c6eabe89b8e2
ammarvehbi <merakli.vehbi@gmail.com>
2019-06-01 14:08:59 +0200

    Loads user & Authenticates token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,26 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
 import { setAlert } from './alert';
+import setAuthToken from '../utils/setAuthToken';
+
+// Load User
+export const loaduser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
