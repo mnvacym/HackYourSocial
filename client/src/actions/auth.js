414986b5fb4f54e619dedf206750e73401bbd52d
ammarvehbi <merakli.vehbi@gmail.com>
2019-06-05 17:02:32 +0200

    Create basic structure of Dashboard component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -96,6 +97,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout User / Clear Profile
 export const logout = () => dispatch => {
-  // dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
