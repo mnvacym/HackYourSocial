ab0621b8c46fc47fa44b658d37fa0904f40dab33
ammarvehbi <merakli.vehbi@gmail.com>
2019-06-12 13:51:34 +0200

    Create Like & Unlike reducers and actions

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => (
+const PostItem = ({
+  addLike,
+  removeLike,
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date },
+}) => (
   <div className="post bg-white p-1 my-1">
     <div>
       <Link to={`/profile/${user}`}>
@@ -17,11 +23,11 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
       <p className="post-date">
         Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
       </p>
-      <button type="button" className="btn btn-light">
+      <button onClick={() => addLike(_id)} type="button" className="btn btn-light">
         <i className="fas fa-thumbs-up" />{' '}
         <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
       </button>
-      <button type="button" className="btn btn-light">
+      <button onClick={() => removeLike(_id)} type="button" className="btn btn-light">
         <i className="fas fa-thumbs-down" />
       </button>
       <Link to={`/posts/${_id}`} class="btn btn-primary">
@@ -43,6 +49,8 @@ PostItem.defaultProps = {
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -51,4 +59,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
+  { addLike, removeLike }
 )(PostItem);
