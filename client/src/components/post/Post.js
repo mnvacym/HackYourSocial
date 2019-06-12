967ab01e56cff073acf330210a40fbbd81138b0a
ammarvehbi <merakli.vehbi@gmail.com>
2019-06-12 22:00:36 +0200

    Create CommentForm & CommentItem components and ADD_COMMENT & REMOVE_COMMENT actions, reducers

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -19,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
