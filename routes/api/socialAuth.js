7fc34b00d5278a391e44bae9553a09b99abaa7c0
YaseirAlkhwalda <yaseir.alkhwalda@gmail.com>
2019-06-21 00:02:34 +0200

    modify socialAuth file

--- routes/api/socialAuth.js
+++ routes/api/socialAuth.js
@@ -10,25 +10,40 @@ router.get(
   })
 );
 // Callback route to redirect to
-router.get('/google/redirect', (req, res) => {
-  res.send('This is callback route for google auth');
-  res.send(req.user); // @Todo - Redirect this user to profile endpoint
-});
+router.get(
+  '/google/redirect',
+  passport.authenticate('google', { failureRedirect: '/login', session: false }),
+  (req, res) => {
+    res.send('This is callback route for google auth');
+    res.send(req.user); // @Todo - Redirect this user to profile endpoint
+    res.json(req.user);
+  }
+);
 
 // Github
 router.get('/github', passport.authenticate('github'));
 // Callback route to redirect to
-router.get('/google/redirect', (req, res) => {
-  res.send('This is callback route for github auth');
-  res.send(req.user); // @Todo - Redirect this user to profile endpoint
-});
+router.get(
+  '/github/redirect',
+  passport.authenticate('google', { failureRedirect: '/login', session: false }),
+  (req, res) => {
+    res.send('This is callback route for github auth');
+    res.send(req.user); // @Todo - Redirect this user to profile endpoint
+    res.json(req.user);
+  }
+);
 
 // Facebook
 router.get('/facebook', passport.authenticate('facebook'));
 // Callback route to redirect to
-router.get('/google/redirect', (req, res) => {
-  res.send('This is callback route for facebook auth');
-  res.send(req.user); // @Todo - Redirect this user to profile endpoint
-});
+router.get(
+  '/facebook/redirect',
+  passport.authenticate('google', { failureRedirect: '/login', session: false }),
+  (req, res) => {
+    res.send('This is callback route for facebook auth');
+    res.send(req.user); // @Todo - Redirect this user to profile endpoint
+    res.json(req.user);
+  }
+);
 
 module.exports = router;
