4e23ae7187805fb1c285a3206d685588420aa96d
kfth <nieuwleven33@gmail.com>
2019-06-22 02:50:41 +0200

    passport github and facebook strategies setup

--- middleware/passportSetup.js
+++ middleware/passportSetup.js
@@ -12,6 +12,60 @@ const generateRandomPass = name => {
   return `${name}123`;
 };
 
+const checkAndCreateUser = async (accessToken, refreshToken, profile, done, account) => {
+  const {
+    id: socialId,
+    displayName: name,
+    emails: [{ value: email }],
+    photos: [{ value: avatar }],
+  } = profile;
+
+  console.log(profile);
+  console.log(name, email, avatar, socialId);
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+
+    //if user does not exist create and save user
+    if (!user) {
+      const password = generateRandomPass(name); // Password is required
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+        social: {
+          [account]: socialId,
+        },
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(user.password, salt);
+      await user.save();
+      console.log('user saved', user);
+    }
+
+    // Return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+      if (err) throw err;
+      console.log('token:', token);
+      done(null, token);
+    });
+  } catch (err) {
+    console.log(err.message);
+    done(err, null);
+  }
+};
+
 // Google Strategy
 passport.use(
   new GoogleStrategy(
@@ -20,103 +74,40 @@ passport.use(
       clientSecret: config.get('google.secret'),
       callbackURL: 'http://localhost:5000/api/auth/social/google/redirect',
     },
-    async (accessToken, refreshToken, profile, done) => {
-      const {
-        id: googleId,
-        displayName: name,
-        emails: [{ value: email }],
-        photos: [{ value: avatar }],
-      } = profile;
-      //const email = profile.emails[0].value;
-      //const googleId = profile.id;
-      console.log(profile);
-      console.log(name, email, avatar, googleId);
-      try {
-        // See if user exists
-        let user = await User.findOne({ email });
-
-        //if user does not exist create and save user
-        if (!user) {
-          const password = generateRandomPass(name); // Password is required
-
-          user = new User({
-            name,
-            email,
-            avatar,
-            password,
-            social: {
-              google: googleId,
-            },
-          });
-
-          // Encrypt password
-          const salt = await bcrypt.genSalt(10);
-
-          user.password = await bcrypt.hash(user.password, salt);
-          await user.save();
-          console.log('user saved', user);
-        }
-
-        // Return jsonwebtoken
-        const payload = {
-          user: {
-            id: user.id,
-          },
-        };
-
-        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
-          if (err) throw err;
-          console.log('token:', token);
-          done(null, token);
-        });
-      } catch (err) {
-        console.log(err.message);
-        done(err, null);
-      }
-    }
-  )
+    (accessToken, refreshToken, profile, done) =>
+      checkAndCreateUser(accessToken, refreshToken, profile, done),
+  ),
 );
 
-/*
 // Facebook Strategy
 passport.use(
   new FacebookStrategy(
     {
-      clientId: config.get('facebook.clientId'),
+      clientID: config.get('facebook.clientId'),
       clientSecret: config.get('facebook.secret'),
-      callback: '/auth/social/facebook/redirect',
+      callbackURL: 'http://localhost:5000/api/auth/social/facebook/redirect',
+      profileFields: ['id', 'displayName', 'photos', 'email'],
     },
-    (accessToken, refreshToken, profile, done) => {
-      // @Todo - Check if user exists, if so let log in user, if not save on db
-      console.log('It is from facebook callback');
-      User.findOne({ facebookId: profile.id }, (err, user) => {
-        console.log(user);
-        return done(err, user);
-      });
-    }
-  )
+    (accessToken, refreshToken, profile, done) =>
+      checkAndCreateUser(accessToken, refreshToken, profile, done),
+  ),
 );
 
 // Github Strategy
 passport.use(
   new GithubStrategy(
     {
-      clientId: config.get('github.clientId'),
+      clientID: config.get('github.clientId'),
       clientSecret: config.get('github.secret'),
-      callback: '/auth/social/github/redirect',
+      callbackURL: 'http://localhost:5000/api/auth/social/github/redirect',
+      scope: 'user:email',
     },
-    (accessToken, refreshToken, profile, done) => {
-      // @Todo - Check if user exists, if so let log in user, if not save on db
-      console.log('It is from github callback');
-      User.findOne({ githubId: profile.id }, (err, user) => {
-        console.log(user);
-        return done(err, user);
-      });
-    }
-  )
+    (accessToken, refreshToken, profile, done) =>
+      checkAndCreateUser(accessToken, refreshToken, profile, done),
+  ),
 );
 
-// Serialization on cookie
+/* Serialization on cookie
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
--- routes/api/socialAuth.js
+++ routes/api/socialAuth.js
@@ -2,48 +2,40 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+const redirectOptions = {
+  failureRedirect: 'http://localhost:3000/',
+  session: false,
+};
+
+const sendToken = (req, res) => {
+  const token = req.user;
+  res.redirect('http://localhost:3000/register?token=' + token);
+};
+
 // Google
 router.get(
   '/google',
   passport.authenticate('google', {
     scope: ['profile', 'email'],
-  })
+  }),
 );
 // Callback route to redirect to
-router.get(
-  '/google/redirect',
-  passport.authenticate('google', { failureRedirect: '/login', session: false }),
-  (req, res) => {
-    //if (!req.token) res.json({ msg: 'No user in request' });
-    const token = req.user;
-    res.redirect('http://localhost:3000/register?token=' + token);
-  }
+router.get('/google/redirect', passport.authenticate('google', redirectOptions), (req, res) =>
+  sendToken(req, res),
 );
 
 // Github
 router.get('/github', passport.authenticate('github'));
 // Callback route to redirect to
-router.get(
-  '/github/redirect',
-  passport.authenticate('google', { failureRedirect: '/login', session: false }),
-  (req, res) => {
-    res.send('This is callback route for github auth');
-    res.send(req.user); // @Todo - Redirect this user to profile endpoint
-    res.json(req.user);
-  }
+router.get('/github/redirect', passport.authenticate('github', redirectOptions), (req, res) =>
+  sendToken(req, res),
 );
 
 // Facebook
 router.get('/facebook', passport.authenticate('facebook'));
 // Callback route to redirect to
-router.get(
-  '/facebook/redirect',
-  passport.authenticate('google', { failureRedirect: '/login', session: false }),
-  (req, res) => {
-    res.send('This is callback route for facebook auth');
-    res.send(req.user); // @Todo - Redirect this user to profile endpoint
-    res.json(req.user);
-  }
+router.get('/facebook/redirect', passport.authenticate('facebook', redirectOptions), (req, res) =>
+  sendToken(req, res),
 );
 
 module.exports = router;
