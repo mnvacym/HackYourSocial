62db78f353eecc4507646d58bc477843c95e7ca3
Bassam <b2dndr1@gmail.com>
2019-06-27 00:16:45 +0200

    Coded by Ahmet, installing react-share, updating PostItem.js & App.css

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,47 +3,81 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike, deletePost } from '../../actions/post';
+import { TwitterShareButton, TwitterIcon, LinkedinShareButton, LinkedinIcon } from 'react-share';
+import { addLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
   addLike,
-  removeLike,
+  // addUnLike,
   deletePost,
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date },
+  post: {
+    _id,
+    title,
+    text,
+    name,
+    avatar,
+    user,
+    likes,
+    // unlikes,
+    comments,
+    date,
+  },
   showActions,
 }) => (
-  <div className="post bg-white p-1 my-1">
+  <div className='post bg-white p-1 my-1'>
     <div>
       <Link to={`/profile/${user}`}>
-        <img className="round-img" src={avatar} alt="" />
+        <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
       </Link>
     </div>
     <div>
-      <p className="my-1">{text}</p>
-      <p className="post-date">
-        Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+      <h2 className='my-1'>{title}</h2>
+      <p className='my-1'>{text}</p>
+      <p className='post-date'>
+        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
 
       {showActions && (
         <Fragment>
-          <button onClick={() => addLike(_id)} type="button" className="btn btn-light">
-            <i className="fas fa-thumbs-up" />{' '}
-            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+          <button onClick={() => addLike(_id)} type='button' className='btn btn-light'>
+            <i className='fas fa-thumbs-up' />{' '}
+            <span>{likes.length >= 0 && <span>{likes.length}</span>}</span>
           </button>
-          <button onClick={() => removeLike(_id)} type="button" className="btn btn-light">
-            <i className="fas fa-thumbs-down" />
-          </button>
-          <Link to={`/posts/${_id}`} className="btn btn-primary">
+          {/* <button onClick={() => addUnLike(_id)} type='button' className='btn btn-light'>
+            <i className='fas fa-thumbs-down' />{' '}
+            <span>{unlikes.length >= 0 && <span>{unlikes.length}</span>}</span>
+          </button> */}
+          <Link to={`/posts/${_id}`} className='btn btn-primary'>
             Discussion{' '}
-            {comments.length > 0 && <span className="comment-count">{comments.length}</span>}
-          </Link>
+            {comments.length > 0 && <span className='comment-count'>{comments.length}</span>}
+          </Link>{' '}
           {!auth.loading && user === auth.user._id && (
-            <button onClick={() => deletePost(_id)} type="button" className="btn btn-danger">
-              <i className="fas fa-times" />
+            <button onClick={() => deletePost(_id)} type='button' className='btn btn-danger'>
+              <i className='fas fa-times' />
             </button>
           )}
+          <div>
+            <h4 className='shr-btn shr-text'>Share on: </h4>
+
+            {/* Share on Twitter:
+            title: Title of the shared page (string)
+            via: (string)
+            hashtags: (array) */}
+            <TwitterShareButton
+              url={`http://localhost:3000/posts/${_id}`}
+              title={title}
+              className='shr-btn twitter'
+            >
+              <TwitterIcon size={40} round={true} />
+            </TwitterShareButton>
+
+            {/* Share on Linkedin */}
+            <LinkedinShareButton url={`http://localhost:3000/posts`} className='shr-btn linkedin'>
+              <LinkedinIcon size={40} round={true} />
+            </LinkedinShareButton>
+          </div>
         </Fragment>
       )}
     </div>
@@ -58,7 +92,7 @@ PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
   addLike: PropTypes.func.isRequired,
-  removeLike: PropTypes.func.isRequired,
+  // unLike: PropTypes.func.isRequired,
   deletePost: PropTypes.func.isRequired,
 };
 
@@ -68,5 +102,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { addLike, removeLike, deletePost }
+  { addLike, deletePost }
 )(PostItem);
