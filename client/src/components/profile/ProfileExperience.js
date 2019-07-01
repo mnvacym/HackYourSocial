3977eaa1699d92b6028f0663955ff9e5347354c8
Bassam <b2dndr1@gmail.com>
2019-07-01 21:44:30 +0200

    adding a line to display the location of each experience, onlythe file ProfileExperience.js has been updated.

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -6,13 +6,16 @@ const ProfileExperience = ({
   experience: { company, title, location, current, to, from, description },
 }) => (
   <div>
-    <h3 className="text-dark">{company}</h3>
+    <h3 className='text-dark'>{company}</h3>
     <p>
-      <Moment format="YYYY/MM/DD">{from}</Moment> -{' '}
-      {!to ? ' Now' : <Moment format="YYYY/MM/DD">{to}</Moment>}
+      <Moment format='YYYY/MM/DD'>{from}</Moment> -{' '}
+      {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
     </p>
     <p>
       <strong>Position: </strong> {title}
+    </p>
+    <p>
+      <strong>Location: </strong> {location}
     </p>
     <p>
       <strong>Description: </strong> {description}
