a1d276dce10f31ea02ea72d34c2e0884d2e5d6f0
Bassam <b2dndr1@gmail.com>
2019-07-03 02:44:45 +0200

    Adding a dual filter for both skill & location

--- client/src/components/profiles/Filtered.js
+++ client/src/components/profiles/Filtered.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import React, { Fragment, useState, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
@@ -19,90 +20,148 @@ const Filtered = ({ getProfiles, profile: { profiles, loading } }) => {
   const [location, setLocation] = useState('');
   const { locationName = '' } = location;
 
-  // value state: to save the value input from the search bar
+  // value: to save the value input from the search bar
   const [value, setValue] = useState('');
-  const { inputValue } = value;
+  const { inputValue, inputValue2 } = value;
 
   // identify which button is clicked to apply the filtering as per it.
   const [activated, setActivated] = useState(false);
-  const { skillActivated } = activated;
+  const { skillActivated, locationActivated, bothActivated } = activated;
 
   // === VARIABLES ===
   let settingState,
     filtered_profiles = [];
 
   // === METHODS ===
-  // dynamic select a fuction to set the state as per the state name clicked.
-  skillActivated ? (settingState = setSkill) : (settingState = setLocation);
+  // dynamic select a fuction to setState as per the filtering button clicked. (single filter)
+  if (!bothActivated) {
+    skillActivated ? (settingState = setSkill) : (settingState = setLocation);
+  }
 
-  // onChange
+  // onChange : works only for single filter not for both
   const onChange = e => settingState({ [e.target.name]: e.target.value });
 
   // click filter button
   const onSubmit = async e => {
     e.preventDefault();
     // save the input value before clearing the field
-    skillActivated
-      ? setValue({ inputValue: skillName.toUpperCase() })
-      : setValue({ inputValue: locationName.toUpperCase() });
+    if (!bothActivated) {
+      skillActivated
+        ? setValue({ inputValue: skillName.toUpperCase() })
+        : setValue({ inputValue: locationName.toUpperCase() });
+    } else {
+      setValue({ inputValue: skillName.toUpperCase(), inputValue2: locationName.toUpperCase() });
+    }
     // clear the input field
     setSkill({ skillName: '' });
     setLocation({ locationName: '' });
   };
 
   // filter profiles by:
-  skillActivated
-    ? // skills
-      // eslint-disable-next-line array-callback-return
+  (function filterByActivatedBtn() {
+    // skills
+    if (skillActivated) {
       profiles.map(profile => {
-        if (profile.skills) {
+        if (profile.skills.length > 0) {
           profile.skills.map(skill => skill.toUpperCase()).includes(inputValue) &&
             filtered_profiles.push(profile);
         }
-      })
-    : // location
-      // eslint-disable-next-line array-callback-return
+      });
+      // location
+    } else if (locationActivated) {
       profiles.map(profile => {
         if (profile.location) {
           profile.location.toUpperCase() === inputValue && filtered_profiles.push(profile);
         }
       });
+      // both skills & location
+    } else if (bothActivated) {
+      profiles.map(profile => {
+        if (profile.location && profile.skills.length > 0) {
+          profile.location.toUpperCase() === inputValue2 &&
+            profile.skills.map(skill => skill.toUpperCase()).includes(inputValue) &&
+            filtered_profiles.push(profile);
+        }
+      });
+    }
+  })();
 
   // click on skill
   const skillOnClick = () => {
-    setActivated({ skillActivated: true, locationActivated: false });
+    setActivated({ skillActivated: true, locationActivated: false, bothActivated: false });
   };
 
   // click on location
   const locationOnClick = () => {
-    setActivated({ skillActivated: false, locationActivated: true });
+    setActivated({ locationActivated: true, skillActivated: false, bothActivated: false });
   };
+
+  // click on both skill & location
+  const bothOnClick = () => {
+    setActivated({ bothActivated: true, skillActivated: false, locationActivated: false });
+  };
+
+  // to display input field for single filter
+  const singleFilter = () => (
+    <input
+      type='text'
+      placeholder={skillActivated ? 'skill' : 'location'}
+      name={skillActivated ? 'skillName' : 'locationName'}
+      value={(skillActivated ? skillName : locationName) || ''}
+      onChange={e => onChange(e)}
+    />
+  );
+
+  // to display input field for dual filter
+  const dualFilter = () => (
+    <div>
+      <input
+        type='text'
+        placeholder={'skill'}
+        name={'skillName'}
+        value={skillName || ''}
+        onChange={e => setSkill({ [e.target.name]: e.target.value })}
+        required
+      />
+      <br />
+      <input
+        type='text'
+        placeholder={'location'}
+        name={'locationName'}
+        value={locationName || ''}
+        onChange={e => setLocation({ [e.target.name]: e.target.value })}
+        required
+      />
+    </div>
+  );
 
   return (
     <Fragment>
       <h4 className='large text-primary'>Filter Profiles by Skill or by Location</h4>
       <div>
         <h2>Filter By</h2>
+        {/* Skill button */}
         <input type='submit' className='btn btn-primary' value='Skill' onClick={skillOnClick} />
+        {/* locatoin button */}
         <input
           type='submit'
           className='btn btn-primary'
           value='Location'
           onClick={locationOnClick}
         />
+        {/* skill & location button */}
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Skill & Location'
+          onClick={bothOnClick}
+        />
       </div>
 
       <form className='form' onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            type='text'
-            placeholder={skillActivated ? 'skill' : 'location'}
-            name={skillActivated ? 'skillName' : 'locationName'}
-            value={(skillActivated ? skillName : locationName) || ''}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        {/* {skillActivated && ( // later improvement
+        <div className='form-group'>{!bothActivated ? singleFilter() : dualFilter()}</div>
+        {/* // later to be improved
+        {skillActivated && ( 
           <small className='form-text'>
             (FOR SKILLS) Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
           </small>
